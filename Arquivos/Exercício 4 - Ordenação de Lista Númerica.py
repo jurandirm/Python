04f74ecdd10eb9f24f12273d247ce9a2d7f9a2cc
Jurandir Marinho <jurandirmarinho13@gmail.com>
2021-09-19 11:11:51 -0300

    Cálculo de Idade
    Algoritmo simples para cálculo de idade

---
+++ Arquivos/Exercício 4 - Ordenação de Lista Númerica.py
@@ -0,0 +1,11 @@
+# Exercício 4 - Ordenação de Lista Númerica
+
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
+n3 = float(input("Digite o terceiro valor: "))
+
+lista = [n1,n2,n3]
+
+lista = sorted(lista)
+
+print(lista)
